b122b522b9dcb0c3598248911952644b55638e96
Kevin Heavey <kevinheavey123@gmail.com>
2022-02-13 12:19:03 +0000

    more rough work

--- test_mnemonic.py
+++ test_mnemonic.py
@@ -1,1 +1,11 @@
 from pybip39 import Mnemonic
+
+
+def test_back_to_back() -> None:
+    m1 = Mnemonic(12, "en")
+    m2 = Mnemonic.from_phrase(m1.phrase, "en")
+    m3 = Mnemonic.from_entropy(m1.entropy, "en")
+    assert m1.entropy == m2.entropy
+    assert m1.entropy == m3.entropy
+    assert m1.phrase == m2.phrase
+    assert m1.phrase == m3.phrase
